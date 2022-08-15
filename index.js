76bc55219683db88a25ca223c8e08d0d71139a34
Justina Mandaro <bautistajustina7@gmail.com>
2022-08-15 13:03:29 -0700

    added code to split and capitilize word

--- index.js
+++ index.js
@@ -11,6 +11,48 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = () => {
+//   return tutorials
+// }
+
+
+function titleCased () {
+  const seperateWords = tutorials.map((tutorials) => {return tutorials.toLowerCase().split(' ')});
+  console.log(seperateWords)
+  
+  for (let word of seperateWords) {
+    //console.log(word)
+   
+    const capitalizeWords = word.map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  })
+  console.log(capitalizeWords)
+  //return(capitalizeWords)
+  }
+  
+  }
+   
+ 
+ // const initialValue = "";
+ 
+//    let stringSum = whatThis2.reduce(
+//   (previousValue, currentValue) => previousValue + currentValue + " ",
+//   initialValue
+// );
+  //return whatThis3
 }
+  // const stringSum = whatThis2.map((whatThis2) => whatThis2.join(" "));
+//console.log(whatThis2)
+
+// const initialValue = 0;
+// const sumWithInitial = whatThis2.reduce(
+//   (previousValue, currentValue) => previousValue + currentValue,
+//   initialValue
+// );
+
+//console.log(whatThis3);
+
+  
+}
+
+titleCased();
